6bd5876ae6ae149253be0f6f8c2955e9a13f97c2
Bojan Jovanovic <bojanj6m@gmail.com>
2017-08-07 17:54:30 -0400

    Configuring Form and Post Route

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models');
+const Page = models.Page;
+const User = models.User;
+
 module.exports = router;
 
 router.get('/', function(req, res){
@@ -7,7 +11,14 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-    console.log(req.body);
+    //console.log(req.body);
+    // let newPage = Page.build({
+    //     title: req.body.title,
+    //     content: req.body.content,
+    //     status: req.body.status
+    // })
+    let newPage = Page.build(req.body);
+    console.log(newPage);
 
 });
 
